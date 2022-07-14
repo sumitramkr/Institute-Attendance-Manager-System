83fc9d6b3d1d46d42a2e5cb33362a1bf91452f3f
sumitramkr <sumitramkr77e@gmail.com>
2022-07-14 12:50:08 +0530

    onchnage

--- client/src/components/Attendance.jsx
+++ client/src/components/Attendance.jsx
@@ -1,24 +1,63 @@
 import React from "react";
 
 function Attendance() {
+  const [attendanceMarking, setAttendanceMarking] = React.useState({
+    cell1: 1,
+  });
+
+  function PresentChange(event) {
+    console.log(event.target.value);
+
+    setAttendanceMarking((attendanceMarking) => {
+      return {
+        ...attendanceMarking,
+        cell1: 1,
+      };
+    });
+    console.log(attendanceMarking);
+  }
+
+  function AbsentChange(event) {
+    console.log(event.target.value);
+    setAttendanceMarking((attendanceMarking) => {
+      return {
+        ...attendanceMarking,
+        cell1: 0,
+      };
+    });
+    console.log(attendanceMarking);
+  }
+
   return (
-    <form>
+    <div>
       <h4>Name</h4>
       <h4>Roll Number</h4>
-      <div>
-        <input
-          type="radio"
-          id="present"
-          name="attendance"
-          value="present"
-          checked
-        />
-        <label for="present">Present</label>
 
-        <input type="radio" id="absent" name="attendance" value="absent" />
-        <label for="absent">Absent</label>
+      <div>
+        <form>
+          <input
+            type="radio"
+            id="present"
+            name="attendance"
+            value="1"
+            checked
+            onChange={PresentChange}
+          />
+          <label for="present">Present</label>
+
+          <input
+            type="radio"
+            id="absent"
+            name="attendance"
+            value="0"
+            onChange={AbsentChange}
+          />
+          <label for="absent">Absent</label>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
 

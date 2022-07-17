0407e0aea435810bb530d65d48ddea2dc6ddb81a
sumitramkr <sumitramkr77e@gmail.com>
2022-07-17 19:27:22 +0530

    tweaked createsheet

--- client/src/components/Attendance.jsx
+++ client/src/components/Attendance.jsx
@@ -7,12 +7,29 @@ function Attendance() {
   const [attedanceList, setAttedanceList] = useState([]);
 
   useEffect(() => {
-    Axios.get("http://localhost:9000/attendance").then((response) => {
-      setAttedanceList(response.data.data.values);
-    });
+    Axios.get("http://localhost:9000/attendance")
+      .then((response) => {
+        console.log(response.data.data.values);
+        setAttedanceList(response.data.data.values);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
   }, []);
 
+  // console.log(attedanceList);
+
   function sendAllValues(event) {
+    console.log("Reached Till post React");
     Axios.post("http://localhost:9000/attendance", { attedanceList });
     // event.preventDefault();
   }
